37157b4f47e9d51a4bb4c6649f272b0722ac4e71
Dede Firmansah <dedefirmansah0508@gmail.com>
2024-11-28 13:30:46 +0700

    test meta

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Metadata } from "next";
+import React from "react";
 
-type Props = {}
+type Props = {};
+export const metadata: Metadata = {
+  title: "My Awesome Website",
+  description: "Welcome to my awesome website built with Next.js",
+  keywords: "Next.js, SEO, website",
+  openGraph: {
+    title: "My Awesome Website",
+    description: "Explore my awesome website",
+    url: "https://my-awesome-website.com",
+    siteName: "My Awesome Website",
+    images: [
+      {
+        url: "https://my-awesome-website.com/images/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Preview Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "My Awesome Website",
+    description: "Welcome to my awesome website",
+    images: "https://my-awesome-website.com/images/twitter-image.jpg",
+  },
+};
 
 export default function Page({}: Props) {
   return (
-    <div>Halaman Blog</div>
-  )
+    <>
+      <div>Halaman Blogs</div>;
+    </>
+  );
 }
