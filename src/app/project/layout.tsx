bb32ab1104b921653f335c70a081752853767ad5
Dede Firmansah <dedefirmansah0508@gmail.com>
2024-12-13 11:34:26 +0700

    fixing params

--- src/app/project/layout.tsx
+++ src/app/project/layout.tsx
@@ -2,12 +2,8 @@ import Breadcrumbs from "@/components/ui/Breadcrumbs";
 
 export default function ProjectLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: { slug: string[] };
 }) {
-  const pathname = params?.slug ? `/${params.slug.join("/")}` : "/";
-
   return <div>{children}</div>;
 }
